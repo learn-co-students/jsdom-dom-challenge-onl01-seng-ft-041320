f2fbd2471663135a49ee489045d9a77d6ac4bd2a
dbhmunro <dbhmunro@gmail.com>
2020-07-23 01:28:59 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,61 +1,89 @@
-"use strict";
-function _toConsumableArray(a){
-    if(Array.isArray(a)){
-        for(var b=0,c=Array(a.length);
-        b<a.length;b++)c[b]=a[b];
-        return c
-    }
-    return Array.from(a)
+const counter=document.getElementById("counter")
+const minus=document.getElementById("minus")
+const plus=document.getElementById("plus")
+const pause=document.getElementById("pause")
+const heart=document.getElementById("heart")
+const commentFormTextField=document.getElementById("comment-input")
+const commentFormSubmit=document.getElementById("submit")
+const toggleObjects=[minus,plus,heart,commentFormTextField,commentFormSubmit]
+const commentForm=document.getElementById("comment-form")
+
+let tick
+let likesList = document.getElementsByClassName("likes")[0]
+
+function counting() {
+    tick = setInterval(upCount, 1000);
 }
 
-var playing=!0,
-timer=function(){
-    return setInterval(function(){
-        var a=document.getElementById("counter"), b=parseInt(a.innerText);
-        a.innerText=b+1
-    },1e3)
-},
-interval=timer(),
-minus=document.getElementById("minus"),
-plus=document.getElementById("plus"),
-heart=document.getElementById("heart"),
-pause=document.getElementById("pause"),
-commentForm=document.getElementsByTagName("form")[0];
+function stopCounting() {
+    clearInterval(tick)
+}
 
-minus.addEventListener("click",function(){
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText);a.innerText=b-1
+function upCount() {
+    n = parseInt(counter.innerText)
+    counter.innerText = n + 1
+}
+
+function downCount() {
+    n = parseInt(counter.innerText)
+    counter.innerText = n - 1
+}
+
+function checkListForId(ulArray, id) {
+    let found = false
+    if (ulArray.length > 0) {
+        for (let li of ulArray) {
+            if (li.id == `${id}`) {
+                found = true
+            }
+        }
+    }
+    return found
+}
+
+document.onload = counting()
+
+minus.addEventListener("click", downCount)
+
+plus.addEventListener("click", upCount)
+
+pause.addEventListener("click", function() {
+    if (pause.innerText == "pause") {
+        stopCounting()
+        toggleObjects.forEach(function(obj) {
+            obj.disabled = true
+        })
+        pause.innerText = "resume"
+    } else {
+        counting()
+        toggleObjects.forEach(function(obj) {
+            obj.disabled = false
+        })
+        pause.innerText = "pause"
+    }
 })
 
-plus.addEventListener("click",function(){
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText);a.innerText=b+1
+heart.addEventListener("click", function(){
+    num = counter.innerText
+    if (checkListForId(likesList.children, num)) {
+        let li = document.getElementById(`${num}`)
+        if (li.children[0].innerText == "1") {
+            li.innerHTML = `${num} has been liked <span>2</span> times`
+        } else {
+            li.children[0].innerText = parseInt(li.children[0].innerText) + 1
+        }
+    } else {
+        likesList.innerHTML += `<li id=${num}>${num} has been liked <span>1</span> time</li>`
+    } 
 })
 
-heart.addEventListener("click",function(){
-    var a=document.getElementById("counter"),
-    b=parseInt(a.innerText),
-    c=document.querySelector(".likes"),
-    d=void 0;
-    if([].concat(_toConsumableArray(c.children)).map(function(a){
-        return parseInt(a.dataset.num)
-    }).includes(b)){
-        d=document.querySelector('[data-num="'+b+'"]');
-        var e=parseInt(d.children[0].innerText);
-        d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"
-    } else(d=document.createElement("li")).setAttribute("data-num",b),d.innerHTML=b+" has been liked <span>1</span> time",c.appendChild(d)
+commentForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    if (commentFormTextField.value != "") {
+        let p = document.createElement("p")
+        let list = document.getElementById("list")
+        list.appendChild(p)
+        p.innerText = commentFormTextField.value
+        commentFormTextField.value = ""
+    }
 })
-
-pause.addEventListener("click",function(){
-    playing?(playing=!1,clearInterval(interval), this.innerText="resume"):(playing=!0,interval=timer(), this.innerText="pause"),
-    [].concat(_toConsumableArray(document.getElementsByTagName("button"))).forEach(function(a){
-        "pause"!==a.id&&(a.disabled=!playing)
-    })
-})
-
-commentForm.addEventListener("submit",function(a){
-    a.preventDefault();
-    var b=this.children[0],c=b.value;b.value="";
-    var d=document.querySelector(".comments"),e=document.createElement("p");
-    e.innerText=c,d.appendChild(e)
-});
